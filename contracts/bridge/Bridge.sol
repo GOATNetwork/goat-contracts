f0ebe03caf2885e74bcf3459b7e4b17a257d9e17
ericlee <eric.l@metis.io>
2024-08-14 23:56:15 +0800

    Remove network import

--- contracts/bridge/Bridge.sol
+++ contracts/bridge/Bridge.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Business Source License 1.1
 pragma solidity ^0.8.24;
 
-import {Network} from "../library/constants/Network.sol";
 import {PreCompiledAddresses} from "../library/constants/Precompiled.sol";
 import {PreDeployedAddresses} from "../library/constants/Predeployed.sol";
 import {Executor} from "../library/constants/Executor.sol";
