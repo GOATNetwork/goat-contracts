79bc610473e71dc81c3e9d0d3487b1403b936464
ericlee <eric.l@metis.io>
2024-08-22 20:50:15 +0800

    Fix lint errors

--- contracts/bitcoin/Bitcoin.sol
+++ contracts/bitcoin/Bitcoin.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Executor} from "../library/constants/Executor.sol";
 import {BaseAccess} from "../library/utils/BaseAccess.sol";
 
 import {IBitcoinBlock} from "../interfaces/bitcoin/Block.sol";
