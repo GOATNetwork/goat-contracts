14a073c638e087363c6d99166c92f08dd657aff0
ericlee <eric.l@metis.io>
2024-09-05 14:44:20 +0800

    feat: add relayer

---
+++ contracts/interfaces/Relayer.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IRelayer {
+    event AddedVoter(bytes20 indexed voter, bytes32 keyHash, uint16 thrs);
+
+    event RemoveVoter(bytes20 indexed voter, uint16 thrs);
+
+    event ChangedThreshold(uint256 threshold);
+
+    function addVoter(bytes20 voter, bytes32 vtkey, uint16 thrs) external;
+
+    function removeVoter(bytes20 voter, uint16 thrs) external;
+
+    function setThreshold(uint16 thrs) external;
+}
