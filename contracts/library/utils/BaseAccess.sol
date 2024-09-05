752527942d7b2432812b6ad5608eb9ed12bcb116
ericlee <eric.l@metis.io>
2024-09-05 19:44:53 +0800

    fix linting

--- contracts/library/utils/BaseAccess.sol
+++ contracts/library/utils/BaseAccess.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.24;
 
 import {Executor} from "../constants/Executor.sol";
-import {PreDeployedAddresses} from "../constants/Predeployed.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BaseAccess {
     error AccessDenied();
