aeb3dade933743810b32fc96565796450c8103d3
Rygoatei <ryan@zkm.io>
2024-09-08 20:12:17 +0800

    chore: adjust config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -233,12 +233,17 @@ const config: HardhatUserConfig = {
       },
     },
   },
+
   networks: {
-  	localhost: {
-		url: "http://127.0.0.1:8545",
-		chainId: 3456,
-		accounts: ["0x0fdce9a033c223590e32ffb24e48d8c66bef942464f7e593925c5317fff0d71e"]
-	}
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 3456,
+      accounts: [
+        "0x0fdce9a033c223590e32ffb24e48d8c66bef942464f7e593925c5317fff0d71e", // replayer and owner
+        // "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", // replayer and owner 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", // GoatFoundation 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+      ],
+    }
   },
   gasReporter: {
     enabled: true,
