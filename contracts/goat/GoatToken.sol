cadd343f64bc79c6187314ca58ddc12ba2309eee
ericlee <eric.l@metis.io>
2024-11-28 09:47:40 +0800

    chore(goat): rename to GOATED

--- contracts/goat/GoatToken.sol
+++ contracts/goat/GoatToken.sol
@@ -8,7 +8,9 @@ import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Vo
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
 contract GoatToken is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
-    constructor(address owner) ERC20("GOAT", "GOAT") ERC20Permit("GOAT") {
+    constructor(
+        address owner
+    ) ERC20("Goat Network", "GOATED") ERC20Permit("GOATED") {
         _mint(owner, 1e9 * 1 ether);
     }
 
